e22aa5aca504493d6037bf96cb1ca08cefd73a55
swhwang <swhwang@wooks-space.me>
2024-10-17 01:36:34 +0900

    3. JWT 토큰을 이용한 사용자 인증 구현

---
+++ jwt_restapi/src/main/java/com/swhwang/jwt_restapi/JwtRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.swhwang.jwt_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRestapiApplication.class, args);
+    }
+
+}
---
+++ jwt_restapi/src/test/java/com/swhwang/jwt_restapi/JwtRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swhwang.jwt_restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
