1a0bdff0b66fcfba888615ea45e4f64766a3c751
swhwang <swhwang@wooks-space.me>
2024-10-16 23:28:03 +0900

    MySQL과 연동하여 사용자 인증 구현

---
+++ mysql_connect/src/main/java/com/swhwang/mysql_connect/MysqlConnectApplication.java
@@ -0,0 +1,13 @@
+package com.swhwang.mysql_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlConnectApplication.class, args);
+    }
+
+}
---
+++ mysql_connect/src/test/java/com/swhwang/mysql_connect/MysqlConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swhwang.mysql_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
